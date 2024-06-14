b55cb6b0727001f8eeca17e9f92c56cfe7dcb118
Pranav Bhavsar <122773238+pranavbhavsar082003@users.noreply.github.com>
2024-06-14 07:29:45 -0400

    views and style added

--- ASSIGNMENT01/routes/index.js
+++ ASSIGNMENT01/routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
+// router objects = controllers
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { layout: "layout" });
 });
-
+/* GET about me */
+router.get("/about", function(req, res, next) {
+  res.render("about", { layout: "layout"});
+});
+/* GET projects. */
+router.get("/projects", function(req, res, next) {
+  res.render("projects", { layout: "layout" });
+});
+/* GET contact me. */
+router.get("/contact", function(req, res, next) {
+  res.render("contact", { layout: "layout" });
+});
 module.exports = router;
